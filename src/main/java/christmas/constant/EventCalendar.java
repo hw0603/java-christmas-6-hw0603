be6ac8aacd7ddaada90b328cd468088105029dc6
HyungUk Ryu <hw0603@naver.com>
2023-11-11 17:10:48 +0900

    feat(EventCalendar): 특별 할인일 데이터 추가
    Enum이 아닌 Set으로 관리

---
+++ src/main/java/christmas/constant/EventCalendar.java
@@ -0,0 +1,9 @@
+package christmas.constant;
+
+import java.util.Set;
+
+public class EventCalendar {
+    public static final Set<Integer> specialDate = Set.of(3, 10, 17, 24, 25, 31);
+    private EventCalendar() {
+    }
+}
