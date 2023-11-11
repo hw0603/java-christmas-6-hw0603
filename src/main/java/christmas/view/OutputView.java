1a56a72caeed6242b2d8e25ac99abca0e1b81652
HyungUk Ryu <hw0603@naver.com>
2023-11-11 18:51:42 +0900

    feat(OutputView): 메뉴 정보 입력 안내 문구 출력 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -6,9 +6,15 @@ public class OutputView {
     public static void printWelcomeMessage() {
         System.out.println(EventPlanner.WELCOME_MSG);
     }
+
     public static void printDateInputDescription() {
         System.out.println(EventPlanner.VISIT_DATE_INPUT_DESC);
     }
+
+    public static void printOrderInputDescription() {
+        System.out.println(EventPlanner.ORDER_INPUT_DESC);
+    }
+
     public static void printExceptionMessage(Exception e) {
         System.out.println(EventPlanner.ERROR_MSG_PREFIX + e.getMessage());
     }
