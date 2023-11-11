974cbf41cdc1b50c22de3e7eb8109df569d0f089
HyungUk Ryu <hw0603@naver.com>
2023-11-11 17:41:11 +0900

    feat(InputView): 식당 예상 방문일을 입력받는 기능 추가

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String intputVisitDate() {
+        return Console.readLine();
+    }
+}
